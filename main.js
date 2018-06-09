16ace67895bdad5299522ed85f770cd54cc1897a
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-06-09 19:10:43 +0900

    Add apple

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ window.onload = function() {
     let game = new Game(320, 320);
     game.fps = 20;
     game.ups = 10;
-    game.preload('chara.png', 'map.png');
+    game.preload('chara.png', 'map.png', 'apple.png');
     const WIDTH = 8;
     const HEIGHT = 8;
     const MAX_X = 32 * 7;
     const MAX_Y = 32 * 7;
     function random_pos() {
-        let r = Math.random() * 10;
-        return r % 8;
+        return ((Math.random() * 10) % 8) * 32;
     }
     const MAP_DATA = [
         [2, 3, 3, 3, 3, 3, 3, 0],
@@ -32,43 +31,72 @@ window.onload = function() {
     const WATER_X = [0, 1, 0, -1, 100];
     const WATER_Y = [-1, 0, 1, 0, 100];
     const INPUT_DV = 4;
+    game.rootScene.addEventListener('touchstart', function(e) {
+        touchX = e.x;
+        touchY = e.y;
+    });
+    game.rootScene.addEventListener('touchmove', function(e) {
+        touchX = e.x;
+        touchY = e.y;
+    });
+    Player = enchant.Class.create(Sprite, {
+        initialize: function() {
+            let game = enchant.Game.instance;
+            Sprite.call(this, 32, 32);
+            this.image = game.assets['chara.png'];
+            this.isMoving = false;
+            this.x = 0;
+            this.y = 0;
+            this.count = 0;
+            this.addEventListener('enterframe', function() {
+                this.count += 1;
+                let dx = 0;
+                let dy = 0;
+                if (game.input.left) {
+                    dx -= INPUT_DV;
+                } else if (game.input.right) {
+                    dx += INPUT_DV;
+                } else if (game.input.up) {
+                    dy -= INPUT_DV;
+                } else if (game.input.down) {
+                    dy += INPUT_DV;
+                }
+                let id = map_id(this.x, this.y);
+                this.x += WATER_X[id];
+                this.y += WATER_Y[id];
+                this.moveBy(dx, dy);
+                this.x = Math.max(0, this.x);
+                this.y = Math.max(0, this.y);
+                this.x = Math.min(this.x, MAX_X);
+                this.y = Math.min(this.y, MAX_Y);
+            })
+        }
+    });
+    Apple = enchant.Class.create(Sprite, {
+        initialize: function () {
+            let game = enchant.Game.instance;
+            Sprite.call(this, 32, 32);
+            this.image = game.assets['apple.png'];
+        }
+    });
     game.onload = function() {
         let map = new Map(32, 32);
         map.image = game.assets['map.png'];
         map.loadData(MAP_DATA);
-        let player = new Sprite(32, 32);
-        player.isMoving = false;
-        player.x = 0;
-        player.y = 0;
-        player.count = 0;
-        player.addEventListener('enterframe', function() {
-            player.count += 1;
-            let dx = 0;
-            let dy = 0;
-            if (game.input.left) {
-                dx -= INPUT_DV;
-            } else if (game.input.right) {
-                dx += INPUT_DV;
-            } else if (game.input.up) {
-                dy -= INPUT_DV;
-            } else if (game.input.down) {
-                dy += INPUT_DV;
+        let player = new Player();
+        let apple = new Apple();
+        apple.x = random_pos();
+        apple.y = random_pos();
+        apple.addEventListener('enterframe', function() {
+            if (player.within(this, 8)) {
+                this.x = random_pos();
+                this.y = random_pos();
             }
-            let id = map_id(this.x, this.y);
-            this.x += WATER_X[id];
-            this.y += WATER_Y[id];
-            this.moveBy(dx, dy);
-            this.x = Math.max(0, this.x);
-            this.y = Math.max(0, this.y);
-            this.x = Math.min(this.x, MAX_X);
-            this.y = Math.min(this.y, MAX_Y);
         });
-        player.image = game.assets['chara.png'];
-        let apple = new Sprite(32, 32);
-
         let stage = new Group();
         stage.addChild(map);
         stage.addChild(player);
+        stage.addChild(apple);
         game.rootScene.addChild(stage);
     };
     game.start();
